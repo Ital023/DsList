23b37d655c37a92bf74a64b90d1b76531378e0cb
Ital023 <contadoitalo911@gmail.com>
2024-09-21 12:51:34 -0300

    initial project

---
+++ backend/src/main/java/io/github/Ital023/DsList_backend/DsListBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.Ital023.DsList_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsListBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsListBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/Ital023/DsList_backend/DsListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Ital023.DsList_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
